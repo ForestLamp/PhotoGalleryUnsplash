19e1afd6f52ca817a2ee8717b63d1247d26bffa6
ForestLamp <y100b@yandex.ru>
2022-05-02 11:40:59 +0300

    Created TabBar Added icons

--- PhotoGalleryUnsplash/MainTabBarController.swift
+++ PhotoGalleryUnsplash/MainTabBarController.swift
@@ -14,6 +14,24 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .gray
+        setupControllers()
+        
+    }
+    
+    // MARK: - private methods
+    
+    private func setupControllers(){
+        let photosVC = generateNavBarController(vc: PhotosCollectionViewController(collectionViewLayout: UICollectionViewFlowLayout()), title: "PhotoGalery", icon: #imageLiteral(resourceName: "photoGalery23x23"))
+        let favoriteVC = generateNavBarController(vc: ViewController(), title: "Favorite", icon: #imageLiteral(resourceName: "favorite23x23"))
+        viewControllers = [photosVC, favoriteVC]
+
+    }
+    
+    private func generateNavBarController(vc: UIViewController, title: String, icon: UIImage) -> UINavigationController {
+        let navController = UINavigationController(rootViewController: vc)
+        navController.title = title
+        navController.tabBarItem.image = icon
+        vc.navigationItem.title = title
+        return navController
     }
 }
--- PhotoGalleryUnsplash/Supporting Files/SceneDelegate.swift
+++ PhotoGalleryUnsplash/Supporting Files/SceneDelegate.swift
@@ -12,10 +12,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
         
 // MARK: Create TabBarController first screen
         
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.rootViewController = MainTabBarController()
