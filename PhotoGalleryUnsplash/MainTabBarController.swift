60b14fe1643e9ce3b40ca208a992b0c1f257ee44
ForestLamp <y100b@yandex.ru>
2022-05-04 10:32:10 +0300

    Marks

--- PhotoGalleryUnsplash/MainTabBarController.swift
+++ PhotoGalleryUnsplash/MainTabBarController.swift
@@ -7,23 +7,23 @@
 
 import UIKit
 
-class MainTabBarController: UITabBarController {
+final class MainTabBarController: UITabBarController {
     
     // MARK: - Life cicle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupControllers()
+        addControllers()
         
     }
     
-    // MARK: - private methods
+    // MARK: - Private methods
     
-    private func setupControllers(){
+    private func addControllers(){
         let photosVC = generateNavBarController(vc: PhotosCollectionViewController(collectionViewLayout: UICollectionViewFlowLayout()), title: "PhotoGalery", icon: #imageLiteral(resourceName: "photoGalery23x23"))
-        let favoriteVC = generateNavBarController(vc: ViewController(), title: "Favorite", icon: #imageLiteral(resourceName: "favorite23x23"))
-        viewControllers = [photosVC, favoriteVC]
+        let favouriteVC = generateNavBarController(vc: ViewController(), title: "Favorite", icon: #imageLiteral(resourceName: "favorite23x23"))
+        viewControllers = [photosVC, favouriteVC]
 
     }
     
--- PhotoGalleryUnsplash/PhotosCollectionViewController.swift
+++ PhotoGalleryUnsplash/PhotosCollectionViewController.swift
@@ -7,79 +7,67 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class PhotosCollectionViewController: UICollectionViewController {
+    
+//MARK: - Private properties
+    
+    private let reuseIdentifier = "Cell"
+    let timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addElementsOnScreen()
+    }
 
+// MARK: - Private methods
+    
+//MARK: Setup UI
+    
+    private func addElementsOnScreen(){
+        setupCollectionView()
+        setupSearchBar()
+    }
+    
+    private func setupCollectionView(){
         collectionView.backgroundColor = .orange
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+    }
+    
+    private func setupSearchBar(){
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.placeholder = "Search picture"
+        self.navigationItem.searchController = searchController
+        self.definesPresentationContext = true
+        searchController.searchBar.delegate = self
     }
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+// MARK: UICollectionViewDataSource
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+
+        return 5
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
     
-        // Configure the cell
+        cell.backgroundColor = .green
     
         return cell
     }
 
-    // MARK: UICollectionViewDelegate
+}
 
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
+// MARK: - Extensions
 
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+extension PhotosCollectionViewController: UISearchBarDelegate {
     
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
     }
-    */
-
 }
